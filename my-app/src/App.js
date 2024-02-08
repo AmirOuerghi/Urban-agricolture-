5d8289b21eb1450be4900c98980221434db84c31
abdelhakbarbouche <barbouchea0@gmail.com>
2024-02-08 16:47:59 +0100

    done home page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,11 +5,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navbar } from './Components/Navbar';
 import Home from './Components/Home';
 import Plants from './Components/Plants';
-import LoginPage from './Components/LoginPage'; 
 import FarmingEquipment from './Components/farmingequipment'; 
 import CartItems from './Components/CartItems';
 import Footer from './Components/Footer';
-import FarmingEquipment from './Components/farmingequipment';
 import LoginPage from './Components/LoginPage';
 
 function App() {
