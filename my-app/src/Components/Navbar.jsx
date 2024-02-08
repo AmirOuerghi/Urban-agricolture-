b66a7a134d3df89e735ba4df54fb0ab869df0579
firaslamouchi21 <firaslamou@gmail.com>
2024-02-08 10:41:09 +0100

    home page routing/new schema

--- my-app/src/Components/Navbar.jsx
+++ my-app/src/Components/Navbar.jsx
@@ -1,9 +1,8 @@
-// Navbar.js
+// Navbar.jsx
 import React from 'react';
 import './Navbar.css';
 import cart_icon from './Assets/cart_icon.png';
-import { Link } from 'react-router-dom';
-import LoginPage from './LoginPage';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 export const Navbar = () => {
   return (
@@ -13,10 +12,10 @@ export const Navbar = () => {
           <p className='p'>URBAN🌾AGRICULTURE</p>
         </div>
         <ul className='nav-menu'>
-          <li className='btn'><button>Medicine💉Animals🇹🇳</button></li>
+          <li className='btn'><Link to="/plants"><button>Plants</button></Link></li> {/* Link to Plants page */}
           <li className='btn'><button>Farming Equipment</button></li>
           <li className='btn'><button>Hiba</button></li>
-          <li className='btn'><Link to="/login"><button>Login</button></Link></li>
+          <li className='btn'><Link to="/login"><button>Login</button></Link></li> {/* Link to Login page */}
           <img src={cart_icon} alt="" />
           <div className="nav-cart-count">0</div>
         </ul>
