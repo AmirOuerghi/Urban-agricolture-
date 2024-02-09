ec23017d952b7b68a17fde462660a7491d7ecaad
AmirOuerghi <ouerghi501@gmail.com>
2024-02-09 10:32:12 +0100

    hmm

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,10 @@
-// App.js
-
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Navbar } from './Components/Navbar';
-import Home from './Components/Home';
-import Plants from './Components/Plants';
-import LoginPage from './Components/LoginPage'; 
-import FarmingEquipment from './Components/farmingequipment'; 
-
+import LoginPage from './Components/LoginPage';
+// import Plants from './Components/Plants'
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/plants" element={<Plants />} />
-          <Route path="/login" element={<LoginPage />} /> 
-          <Route path="/farming-equipment" element={<FarmingEquipment />} /> 
-        </Routes>
-      </BrowserRouter>
+      <LoginPage />
     </div>
   );
 }
