8c1576adcc8fe142aa0cd738cf32fa76823f4dcb
firaslamouchi21 <firaslamou@gmail.com>
2024-02-12 08:45:59 +0100

    fixes

--- my-app/src/Components/Home.jsx
+++ my-app/src/Components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
+
 const pages = [
   {
     name: 'Plants',
@@ -22,6 +23,13 @@ const pages = [
     image: '',
     description: 'Unlock the secrets to creating stunning outdoor spaces with inspiring landscaping ideas and design tips. Whether you’re revamping your backyard, planning a new garden layout, or enhancing your property’s curb appeal, this page provides a wealth of inspiration for transforming your outdoor environment. Discover creative landscape designs, learn about plant combinations and hardscape elements, and find practical advice for executing your landscaping projects with style and flair.',
     link: '/landscaping-ideas'
+  },
+  {
+    name: 'Veterinarians for Animals',
+    type: 'Type 4',
+    image: '',
+    description: 'Find reliable veterinarians to care for your beloved animals. Whether you have pets or livestock, ensuring their health and well-being is essential. This page connects you with experienced professionals who specialize in various animal species, offering medical care, advice, and support to keep your furry or feathered friends happy and healthy.',
+    link: '/veterinarians'
   }
 ];
 
